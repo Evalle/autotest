bb4a35db577879d9f9718108761088d62d680b96
evalle <shmarnev@gmail.com>
2016-06-15 11:51:58 +0200

    Added argument option

--- crowbar.py
+++ crowbar.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 from splinter import Browser
 
 class Colors:
@@ -16,13 +17,27 @@ yellow = Colors.YELLOW
 red = Colors.RED
 end = Colors.END
 
+# argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument('--port', '-p',
+        help = "crowbar port, for example 3000, 8080")
+
+args = parser.parse_args()
+user_input = args.port
+
+# check that input has an argument
+if user_input is None:
+    print("You should run this program with an argument, run 'gordon --h' for more information")
+    sys.exit()
+
 browser = Browser('phantomjs')
 
 errors = 0
 
-url = "http://crowbar:crowbar@127.0.0.10:3000"
+url = "http://crowbar:crowbar@127.0.0.10:" + user_input
 browser.visit(url)
 
+# checking existence of nodes
 def check_text(text):
 
     global errors
@@ -33,10 +48,11 @@ def check_text(text):
         print(text + ' test ' + red + 'FAILED' + end)
         errors += 1 
 
+# check that we can create a new group
+
 
 print("1st group of tests")
 print('=========')
-
 check_text('3 nodes')
 check_text('crowbar')
 check_text('dashboard')
