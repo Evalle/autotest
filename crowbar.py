5a00bf143ea77e466870e058a7c9d541d6c95c43
evalle <shmarnev@gmail.com>
2016-06-15 11:15:28 +0200

    Refactoring

--- crowbar.py
+++ crowbar.py
@@ -1,17 +1,40 @@
 import sys
 from splinter import Browser
 
+class Colors:
+
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    END = '\033[0m'
+
+# colors
+blue = Colors.BLUE
+green = Colors.GREEN
+yellow = Colors.YELLOW
+red = Colors.RED
+end = Colors.END
+
 browser = Browser('phantomjs')
 
 errors = 0
 
-url = "http://crowbar:crowbar@127.0.0.10:8080"
+url = "http://crowbar:crowbar@127.0.0.10:3000"
 browser.visit(url)
-if browser.is_text_present('2 nodes'):
-    print("Yes, all 3 nodes are present")
-else:
-    print("No, nodes are not present")
-    errors += 1 
 
-browser.quit()
-print("Errors:" + str(errors))
+def check_text(text):
+    
+    if browser.is_text_present(text):
+        print(text + ' test ' + green + 'PASSED' + end)
+    else:
+        print(text + ' test' + red + 'FAILED' + end)
+        errors += 1 
+
+check_text('3 nodes')
+check_text('crowbar')
+check_text('dashboard')
+
+print()
+print('=======')
+print("Errors: " + str(errors))
